691c2690fd9f20cbc2c84a81a11f296ab505c62a
Cole Savage <colesavage02@gmail.com>
2019-11-05 10:09:46 -0800

    Created KDE library for kernels. Started creating sample data to test on for arduino. Ran in to problems with pointers :(

--- Base_Sketch/Base_Sketch.ino
+++ Base_Sketch/Base_Sketch.ino
@@ -1,11 +1,15 @@
-#include "KDE.h"
+#include <KDE.h>
 
+float *dataPtr;
 void setup() {
   // put your setup code here, to run once:
-  
+  float data[] = {0.0f,0.1f,0.1f,0.5f,0.5f,0.5f,0.7f,0.7f,1.0f,2.0f,2.1f,2.1f,2.5f,2.5f,2.5f,2.7f,2.7f,3.0f};
+  dataPtr = data;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  for(int i = 0; i < sizeof(&dataPtr); i++) {
+    printf((*dataPtr)[i]);
+  }
 }
