11e24fa42d1089259ff8a6d5e6c9551c6239f368
Cole Savage <colesavage02@gmail.com>
2019-11-06 10:31:44 -0800

    Contiued implementing java Kernel algorithm into arduino. Figured out how to use a pointer to reference a list (so that the data only has to be stored once)

--- Base_Sketch/Base_Sketch.ino
+++ Base_Sketch/Base_Sketch.ino
@@ -1,15 +1,31 @@
 #include <KDE.h>
 
 float *dataPtr;
+Kernel *kernelPtr;
+int dataSize;
+float h;
+
 void setup() {
   // put your setup code here, to run once:
+  h = 0;
   float data[] = {0.0f,0.1f,0.1f,0.5f,0.5f,0.5f,0.7f,0.7f,1.0f,2.0f,2.1f,2.1f,2.5f,2.5f,2.5f,2.7f,2.7f,3.0f};
-  dataPtr = data;
+  dataSize = sizeof(data)/sizeof(data[0]);
+  dataPtr = &data[0];
+
+  Kernel kernels[dataSize];
+ 
+  Serial.begin(9600);
+
+  float *dataPtrCpy = dataPtr;
+
+  for(unsigned int i = 0; i < dataSize; i++) {
+    Serial.println(*dataPtrCpy);
+    dataPtrCpy++;
+  }
+
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i = 0; i < sizeof(&dataPtr); i++) {
-    printf((*dataPtr)[i]);
-  }
+  // put your main code here, to run repeatedly:  
 }
