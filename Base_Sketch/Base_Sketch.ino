5d61419899ef30143d8219aa6237669fe2347455
Cole Savage <colesavage02@gmail.com>
2019-11-12 10:10:05 -0800

    continued working on findMins function. Have to copy contents of an array to another array because there are no variable length arrays in c++ :(

--- Base_Sketch/Base_Sketch.ino
+++ Base_Sketch/Base_Sketch.ino
@@ -5,6 +5,8 @@ float *dataPtr;
 Kernel *kernelPtr;
 int dataSize;
 const float H = 0.5;
+const float MAX_FLOAT_VAL = 3.4028235e38;
+const float MIN_FLOAT_VAL = -MAX_FLOAT_VAL;
 
 void setup() {
   // put your setup code here, to run once:
@@ -24,7 +26,10 @@ void setup() {
     dataPtrCpy++;
   }
 
+  Kernel *kernelPtr = &kernels[0];
+  float *outputPtr;
 
+  findMin(kernelPtr, dataSize, 0.0, 3.0, 0.1, 0.01, outputPtr);
   
 };
 
@@ -32,8 +37,84 @@ void loop() {
   // put your main code here, to run repeatedly:  
 }
 
-float max(float *arrayStart, unsigned int arrayLength) {
-  float *cpyPtr = arrayStart;
-  for(i = 1; i < arrayLength; i++) {
+void findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound, float upperBound, float algStep, float lossThreshold, float *outputPtr) {
+  Kernel *kernelPtrCpy = kernelPtr;
+  float currentX = lowerBound;
+  float lastValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
+  currentX += algStep;
+  float currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
+  float delta = currentValue - lastValue;
+
+  while(delta == 0.0) {
+    currentX += algStep;
+    lastValue = currentValue;
+    currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
+    delta = currentValue - lastValue;
   }
+
+  currentX += algStep;
+  lastValue = currentValue;
+  currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
+  delta = currentValue - lastValue;
+
+  int lastSgn = signum(delta);
+  int sgn = lastSgn;
+
+  unsigned int arrSize = 0;
+  float *minRanges = new float[0];
+  float *maxRanges = new float[0];
+
+  while(currentX <= upperBound) {
+    lastSgn = sgn;
+    currentX += algStep;
+    lastValue = currentValue;
+
+    currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
+    delta = currentValue - lastValue;
+    sgn = signum(delta);
+
+    if(sgn - lastSgn > 0) {
+      arrSize++;
+      float *newMinArr = new float[arrSize];
+      float *newMaxArr = new float[arrSize];
+      cpyArrayAndAdd(minRanges, arrSize - 1, currentX - algStep, newMinArr);
+    }
+  }
+}
+
+float maxArray(float *arrayStart, unsigned int arrayLength) {
+  float *cpyPtr = arrayStart;
+  float maxVal = MIN_FLOAT_VAL;
+  for(unsigned int i = 0; i < arrayLength; i++) {
+    maxVal = max(*cpyPtr, maxVal);
+    cpyPtr++;
+  }
+  return maxVal;
+}
+
+float minArray(float *arrayStart, unsigned int arrayLength) {
+  float *cpyPtr = arrayStart;
+  float minVal = MAX_FLOAT_VAL;
+  for(unsigned int i = 0; i < arrayLength; i++) {
+    minVal = min(*cpyPtr, minVal);
+    cpyPtr++;
+  }
+  return minVal;
+}
+
+void cpyArrayAndAdd(float *arrInPtr, unsigned int arrInSize, float valToAdd, float *outputPtr) {
+  float *cpyArrInPtr = arrInPtr;
+  float *cpyArrOutPtr = outputPtr;
+  for(int i = 0; i < arrInSize; i++) {
+
+    *cpyArrOutPtr = *cpyArrInPtr;
+    
+    cpyArrInPtr++;
+    cpyArrOutPtr++;
+  }
+  *cpyArrOutPtr = valToAdd;
+}
+
+int signum(float val) {
+    return (0.0 < val) - (val < 0.0);
 }
