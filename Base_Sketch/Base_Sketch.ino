9ef3289485f5fd7467572f4362fc2c919c329da2
Cole Savage <colesavage02@gmail.com>
2019-12-17 10:09:46 -0800

    Added code from stackoverflow for file reading. It is still broken

--- Base_Sketch/Base_Sketch.ino
+++ Base_Sketch/Base_Sketch.ino
@@ -16,6 +16,7 @@ unsigned const int MAX_NUMBER_OF_FILES = 3;
 //PURE EVIL
 unsigned int sizeOfData = 0;
 long timeDataPtGlobal;
+File file;
 
 //ML Vars
 float *dataPtr;
@@ -41,26 +42,26 @@ void setup() {
     while (1);
   }
   Serial.println("initialization done.");
- 
+
   //KDE stuff
-  float data[] = {0.0f,0.1f,0.1f,0.5f,0.5f,0.5f,0.7f,0.7f,1.0f,2.0f,2.1f,2.1f,2.5f,2.5f,2.5f,2.7f,2.7f,3.0f};
-  dataSize = sizeof(data)/sizeof(data[0]);
+  float data[] = {0.0f, 0.1f, 0.1f, 0.5f, 0.5f, 0.5f, 0.7f, 0.7f, 1.0f, 2.0f, 2.1f, 2.1f, 2.5f, 2.5f, 2.5f, 2.7f, 2.7f, 3.0f};
+  dataSize = sizeof(data) / sizeof(data[0]);
   dataPtr = &data[0];
 
   Kernel kernels[dataSize];
- 
+
   Serial.begin(9600);
 
   float *dataPtrCpy = dataPtr;
 
   //Generate Kernels
-  for(unsigned int i = 0; i < dataSize; i++) {   
+  for (unsigned int i = 0; i < dataSize; i++) {
     kernels[i] = *(new Kernel(*dataPtrCpy, H));
     dataPtrCpy++;
   }
-  Kernel *kernelPtr = &kernels[0];  
+  Kernel *kernelPtr = &kernels[0];
   unsigned int outputSize = findMin(kernelPtr, dataSize, 0.0, 3.0, 0.1, 0.01);
-  for(int i = 0; i < outputSize; i++) {
+  for (int i = 0; i < outputSize; i++) {
     //Serial.println(output[i]);
   }
   state = LEARN;
@@ -69,39 +70,39 @@ void setup() {
 void loop() {
 
   //Datacollect state is WIP
-  if(state == DATACOLLECT) {
+  if (state == DATACOLLECT) {
     unsigned int times = fmod(floor(round(millis()) / fPEM), MAX_NUMBER_OF_FILES);
     Serial.println(times);
-    if(allowWrite){
+    if (allowWrite) {
       //If anything is typed in the serial monitor stop writing
-      if(Serial.read() != -1){
-       allowWrite = false;
+      if (Serial.read() != -1) {
+        allowWrite = false;
       }
-      if(lastFile != times) {
+      if (lastFile != times) {
         myFile.close();
- 
+
         lastFile = times;
 
-        if(SD.exists("ms_" + (String) times + ".txt")) {
+        if (SD.exists("ms_" + (String) times + ".txt")) {
           SD.remove("ms_" + (String) times + ".txt");
         }
-        
+
         myFile = SD.open("ms_" + (String) times + ".txt", FILE_WRITE);
       }
-      else{
-          if (myFile) {
-            Serial.println("Wrote to file");
-            myFile.println("time = " + (String) round(millis()));
-         } else {
-            Serial.println((String) "error opening " + (String) "mills: " + (String) times + (String) ".txt");
-         }
-       }
+      else {
+        if (myFile) {
+          Serial.println("Wrote to file");
+          myFile.println("time = " + (String) round(millis()));
+        } else {
+          Serial.println((String) "error opening " + (String) "mills: " + (String) times + (String) ".txt");
+        }
+      }
     }
-    else{
+    else {
       myFile.close();
     }
   }
-  else if(state == LEARN) {
+  else if (state == LEARN) {
     dataSize = getNumDataPoints();
     unsigned long timeData[dataSize];
     long timeData2[dataSize];
@@ -113,129 +114,73 @@ void loop() {
 
 void getAllData(long *timeDataPtr, float *amperageDataPtr, unsigned int listSize) {
 
-  long timeArr[listSize];
-  float ampArr[listSize];
+  long timeArr[18];
   unsigned int tracker = 0;
 
-  for(unsigned int i = 0; i < MAX_NUMBER_OF_FILES; i++) {
-    
-    myFile2 = SD.open("MS_" + (String) i + ".txt" +'\0', FILE_READ);
-    Serial.println("ran " + (String) i);
-    Serial.println((String)i + '\0');
-
-    if(myFile2) {
-
-      unsigned int idx = 0;
-      unsigned int newLineCount = 0;
-      char charArray[myFile2.available()/sizeof('\n')];
-      while(myFile2.available()) {
-        char inChar = myFile.read();
-        charArray[idx] = inChar;
-        idx++;
-        if(inChar == '\n') {
-          newLineCount++;
-        }
-      }
-      myFile2.close();
-
-      continue;
-
-      unsigned int count = 0;
-      unsigned int newLineIdxes[newLineCount + 1];
-      for(unsigned int i = 0; i < sizeof(charArray)/sizeof(charArray[0]); i++) {
-        if(charArray[i] == '\n') {
-          newLineIdxes[count] = i;
-        }
-      }      newLineIdxes[count] = idx;
-      count++;
-      
-      unsigned int startingIdx = 0;
-      //For each newline
-      for(unsigned int i = 0; i < count; i++) {
-        unsigned int newLineIdx = newLineIdxes[i];
-        char val[newLineIdx - startingIdx + 1];
-
-        unsigned int commaIdx = 0;
-        for(unsigned int j = startingIdx; j < newLineIdx; j++) {
-          if(charArray[j] == ',') {
-            commaIdx = j;
-          }
-        }
-
-        char timeData[commaIdx-startingIdx + 1];
-        char ampData[newLineIdx - (commaIdx + 1)];
-        //For each char between the start index and the next newline
-        for(unsigned int j = startingIdx; j < newLineIdx; j++) {
-          val[j - startingIdx] = charArray[j];
-        }
-
-        for(unsigned int k = startingIdx; k < commaIdx; k++) {
-          timeData[k - startingIdx] = charArray[k];
-        }
-       
-        for(unsigned int m = commaIdx + 1; m < newLineIdx - 1; m++) {
-          ampData[m - commaIdx - 1] = charArray[m];
-        }
-
-        timeData[commaIdx - startingIdx] = '\0';
+  for (unsigned int i = 0; i < MAX_NUMBER_OF_FILES; i++) {
 
 
-        
-        //char timeDataCpy[commaIdx-startingIdx + 1];
-        //copy(timeData, timeDataCpy, commaIdx-startingIdx + 1);
 
-
-        char *endPtr;
-        timeDataPtGlobal = strtol(timeData, &endPtr, 10);
-
-        if(!*endPtr) {
-          Serial.println("GOOD :)");
-        }
-        else {
-          Serial.println("BAD");
-        }
-
-        //Serial.println(String(timeDataPtGlobal));
-        //timeArr[tracker] = timeDataPtGlobal;
-        //*timeArrPtr = timePt;
-        //memcpy(&timeArr[tracker],&timePt,sizeof(timePt));
-        tracker++;
-        
-        if(i == count - 1) {
-          char ampDataFinal[newLineIdx - commaIdx];
-          for(unsigned int i = 0; i < sizeof(ampData)/sizeof(ampData[0]); i++) {
-            ampDataFinal[i] = ampData[i];
-          }
-
-          ampDataFinal[newLineIdx - commaIdx - 2] = charArray[newLineIdx - 1];
-          ampDataFinal[newLineIdx - commaIdx - 1] = '\0';
-        }
-        
-        ampData[newLineIdx - 1 - (commaIdx + 1)] = '\0';
-
-        val[newLineIdx - startingIdx] = '\0';
-        //Serial.println(ampData);
-        startingIdx = newLineIdx + 1;
-      }
+    file = SD.open("MS_" + String(i) + ".txt", FILE_READ);
+    if (!file) {
+      Serial.println("open error");
+      return;
     }
-    else {
-      Serial.println("error opening MS_" + (String) i + ".txt");
+    long x;
+    float y;
+    while (readVals(&x, &y)) {
+      Serial.print("x: ");
+      Serial.println(x);
+      //timeArr[tracker] = x;
+      tracker++;
+      Serial.print("y: ");
+      Serial.println(y);
+      Serial.println();
     }
-  }
+    Serial.println("Done");
+  
+}
 
-  /*for(unsigned int i = 0; i < sizeof(timeArr)/sizeof(timeArr[0]); i++) {
-    Serial.println("time "+(String)timeArr[i]);
+/*for(unsigned int i = 0; i < sizeof(timeArr)/sizeof(timeArr[0]); i++) {
+  Serial.println("time "+(String)timeArr[i]);
   }*/
+}
+
+bool readLine(File &f, char* line, size_t maxLen) {
+  for (size_t n = 0; n < maxLen; n++) {
+    int c = f.read();
+    if ( c < 0 && n == 0) return false;  // EOF
+    if (c < 0 || c == '\n') {
+      line[n] = 0;
+      return true;
+    }
+    line[n] = c;
+  }
+  return false; // line too long
+}
+
+bool readVals(long* v1, float* v2) {
+  char line[40], *ptr, *str;
+  if (!readLine(file, line, sizeof(line))) {
+    return false;  // EOF or too long
+  }
+  *v1 = strtol(line, &ptr, 10);
+  if (ptr == line) return false;  // bad number if equal
+  while (*ptr) {
+    if (*ptr++ == ',') break;
+  }
+  *v2 = (float) strtod(ptr, &str);
+  return str != ptr;  // true if number found
 }
 
 unsigned int getNumDataPoints() {
   unsigned int numDataPoints = 0;
-  
-  for(int i = 0; i < MAX_NUMBER_OF_FILES; i++) {
-    myFile3 = SD.open("MS_" + (String) i + ".txt", FILE_READ);
-    if(myFile3) {
-      while(myFile3.available()) {
-        if(myFile3.read() == '\n') {
+
+  for (int i = 0; i < MAX_NUMBER_OF_FILES; i++) {
+    myFile3 = SD.open("MS_" + String(i) + ".txt", FILE_READ);
+    if (myFile3) {
+      while (myFile3.available()) {
+        if (myFile3.read() == ',') {
           numDataPoints++;
         }
       }
@@ -252,11 +197,11 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
   Kernel *kernelPtrCpy = kernelPtr;
   float currentX = lowerBound;
   float lastValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
-  currentX += algStep/2;
+  currentX += algStep / 2;
   float currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
   float delta = currentValue - lastValue;
 
-  while(delta == 0.0) {
+  while (delta == 0.0) {
     currentX += algStep;
     lastValue = currentValue;
     currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, currentX);
@@ -280,7 +225,7 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
   float currentValueCpy = currentValue;
 
   //Find how many mins there are
-  while(currentXCpy < upperBound) {
+  while (currentXCpy < upperBound) {
     lastSgnCpy = sgnCpy;
     currentXCpy += algStep;
     lastValueCpy = currentValueCpy;
@@ -289,16 +234,16 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
     delta = currentValueCpy - lastValueCpy;
     sgnCpy = signum(delta);
 
-    if(sgnCpy - lastSgnCpy > 0) {
+    if (sgnCpy - lastSgnCpy > 0) {
       arrSize++;
     }
   }
-  
+
   float maxRanges[arrSize];
   unsigned int index = 0;
 
   //Find the upper bound of all the mins
-  while(currentX < upperBound) {
+  while (currentX < upperBound) {
     lastSgn = sgn;
     currentX += algStep;
     lastValue = currentValue;
@@ -307,7 +252,7 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
     delta = currentValue - lastValue;
     sgn = signum(delta);
 
-    if(sgn - lastSgn > 0) {
+    if (sgn - lastSgn > 0) {
       maxRanges[index] = currentX;
       index++;
     }
@@ -320,15 +265,15 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
   unsigned int usedFloorVals = 0;
 
   //Refine the estimated mins
-  for(unsigned int i = 0; i < arrSize; i++) {
-    lastValue = Kernel::kernelConsensus(kernelPtr, dataSize, maxRanges[i]-algStep);
-    float mid = (2*maxRanges[i] - algStep)/2;
+  for (unsigned int i = 0; i < arrSize; i++) {
+    lastValue = Kernel::kernelConsensus(kernelPtr, dataSize, maxRanges[i] - algStep);
+    float mid = (2 * maxRanges[i] - algStep) / 2;
     currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, mid);
     float lBound = maxRanges[i] - algStep;
     float uBound = maxRanges[i];
-    while(abs(currentValue-lastValue) > lossThreshold) {
-      float rmid = (mid + uBound)/2;
-      float lmid = (lBound + mid)/2;
+    while (abs(currentValue - lastValue) > lossThreshold) {
+      float rmid = (mid + uBound) / 2;
+      float lmid = (lBound + mid) / 2;
 
       float rmidVal = Kernel::kernelConsensus(kernelPtr, dataSize, rmid);
       float lmidVal = Kernel::kernelConsensus(kernelPtr, dataSize, lmid);
@@ -338,13 +283,13 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
       lastValue = currentValue;
       currentValue = Kernel::kernelConsensus(kernelPtr, dataSize, mid);
     }
-    if(abs(currentValue - lastValue) == 0) {
+    if (abs(currentValue - lastValue) == 0) {
       flooredDividers[usedFloorVals] = mid;
       usedFloorVals++;
       lastMidFloored = true;
     }
-    else if(lastMidFloored) {
-      dividers[usedVals] = (flooredDividers[usedFloorVals - 1] + mid)/2;
+    else if (lastMidFloored) {
+      dividers[usedVals] = (flooredDividers[usedFloorVals - 1] + mid) / 2;
       usedVals++;
       lastMidFloored = false;
     }
@@ -354,8 +299,8 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
     }
   }
 
-  if(usedVals < 32) {
-    for(int i = 0; i < usedVals; i++) {
+  if (usedVals < 32) {
+    for (int i = 0; i < usedVals; i++) {
       output[i] = dividers[i];
     }
   }
@@ -367,9 +312,9 @@ unsigned int findMin(Kernel *kernelPtr, unsigned int dataSize, float lowerBound,
 }
 
 void copy(char* src, char* dst, unsigned int len) {
-    memcpy(dst, src, sizeof(src[0])*len);
+  memcpy(dst, src, sizeof(src[0])*len);
 }
 
 int signum(float val) {
-    return (0.0 < val) - (val < 0.0);
+  return (0.0 < val) - (val < 0.0);
 }
--- FileReadingDontDie/FileReadingDontDie.ino
+++ FileReadingDontDie/FileReadingDontDie.ino
@@ -1,97 +1,65 @@
-#include <SPI.h>
 #include <SD.h>
-#define CS_PIN 10
-
 File file;
 
-/*
- * Read a file one field at a time.
- *
- * file - File to read.
- *
- * str - Character array for the field.
- *
- * size - Size of str array.
- *
- * delim - String containing field delimiters.
- *
- * return - length of field including terminating delimiter.
- *
- * Note, the last character of str will not be a delimiter if
- * a read error occurs, the field is too long, or the file
- * does not end with a delimiter.  Consider this an error
- * if not at end-of-file.
- *
- */
-size_t readField(File* file, char* str, size_t size, char* delim) {
-  char ch;
-  size_t n = 0;
-  while ((n + 1) < size && file->read(&ch, 1) == 1) {
-    // Delete CR.
-    if (ch == '\r') {
-      continue;
+bool readLine(File &f, char* line, size_t maxLen) {
+  for (size_t n = 0; n < maxLen; n++) {
+    int c = f.read();
+    if ( c < 0 && n == 0) return false;  // EOF
+    if (c < 0 || c == '\n') {
+      line[n] = 0;
+      return true;
     }
-    str[n++] = ch;
-    if (strchr(delim, ch)) {
-        break;
-    }
+    line[n] = c;
   }
-  str[n] = '\0';
-  return n;
+  return false; // line too long
 }
-//------------------------------------------------------------------------------
-#define errorHalt(msg) {Serial.println(F(msg)); while(1);}
-//------------------------------------------------------------------------------
+
+bool readVals(long* v1, float* v2) {
+  char line[40], *ptr, *str;
+  if (!readLine(file, line, sizeof(line))) {
+    return false;  // EOF or too long
+  }
+  *v1 = strtol(line, &ptr, 10);
+  if (ptr == line) return false;  // bad number if equal
+  while (*ptr) {
+    if (*ptr++ == ',') break;
+  }
+  *v2 = (float) strtod(ptr, &str);
+  return str != ptr;  // true if number found
+}
+
 void setup() {
+  long x;
+  float y;
   Serial.begin(9600);
-
-  // Initialize the SD.
-  if (!SD.begin(CS_PIN)) errorHalt("begin failed");
-
-  // Create or open the file.
-  file = SD.open("READTEST.TXT", FILE_WRITE);
-  if (!file) errorHalt("open failed");
-
-  // Rewind file so test data is not appended.
-  file.seek(0);
-
-  // Write test data.
-  file.print(F(
-    "field_1_1,field_1_2,field_1_3\r\n"
-    "field_2_1,field_2_2,field_2_3\r\n"
-    "field_3_1,field_3_2\r\n"           // missing a field
-    "field_4_1,field_4_2,field_4_3\r\n"
-    "field_5_1,field_5_2,field_5_3"     // no delimiter
-    ));
-
-  // Rewind the file for read.
-  file.seek(0);
-
-  size_t n;      // Length of returned field with delimiter.
-  char str[20];  // Must hold longest field with delimiter and zero byte.
-  
-  // Read the file and print fields.
-  while (true) {
-    n = readField(&file, str, sizeof(str), ",\n");
-
-    // done if Error or at EOF.
-    if (n == 0) break;
-
-    // Print the type of delimiter.
-    if (str[n-1] == ',' || str[n-1] == '\n') {
-      Serial.print(str[n-1] == ',' ? F("comma: ") : F("endl:  "));
-      
-      // Remove the delimiter.
-      str[n-1] = 0;
-    } else {
-      // At eof, too long, or read error.  Too long is error.
-      Serial.print(file.available() ? F("error: ") : F("eof:   "));
-    }
-    // Print the field.
-    Serial.println(str);
+  if (!SD.begin(4)) {
+    Serial.println("begin error");
+    return;
   }
-  file.close();
+
+  long timeArr[18];
+  unsigned int tracker = 0;
+  for (unsigned int i = 0; i < 3; i++) {
+    
+    file = SD.open("MS_"+String(i)+".txt", FILE_READ);
+    if (!file) {
+      Serial.println("open error");
+      return;
+    }
+    while (readVals(&x, &y)) {
+      Serial.print("x: ");
+      Serial.println(x);
+      timeArr[tracker] = x;
+      tracker++;
+      Serial.print("y: ");
+      Serial.println(y);
+      Serial.println();
+    }
+    Serial.println("Done");
+  }
+  for(unsigned int j = 0; j < sizeof(timeArr)/sizeof(timeArr[0]); j++) {
+      Serial.print("time: ");
+      Serial.println(String(timeArr[j]));
+  }
 }
-//------------------------------------------------------------------------------
-void loop() {
-}
+void loop() {}
